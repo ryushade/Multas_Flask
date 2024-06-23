e37d529cbcda3ec198345d6c5f2655f6b3514740
ryushade <77291154@usat.pe>
2024-06-23 01:48:58 -0500

    a

--- bd.py
+++ bd.py
@@ -4,4 +4,4 @@ def obtener_conexion():
     return pymysql.connect(host='semultasperu.mysql.pythonanywhere-services.com',
                                 user='semultasperu',
                                 password='23bf1a0d2',
-                                db='semultasperu$multas')
+                                db='semultasperu$multasperu')
